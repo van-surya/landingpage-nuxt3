e1d43f7eb19f7323f6c4b1631d69ac13441105df
surevan08 <suryavan08@gmail.com>
2023-11-09 21:53:58 +0700

    Setup

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <nuxt-link class="navbar-brand" to="/">Navbar</nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <nuxt-link class="nav-link active" aria-current="page" to="/"
+                >Home</nuxt-link
+              >
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/portfolio">Portfolio</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!--- render page -->
+    <slot />
+  </div>
+</template>
+ 
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+//meta title
+useHead({
+  title: "Welcome - Surya Van",
+});
+</script>
+
+<template>
+  <div class="hero">
+    <div class="container">
+      <div class="hero_caption">
+        <h1>Loream</h1>
+        <div class="hero_icon">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
