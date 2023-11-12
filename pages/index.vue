5399fd18a36683d83cc480d769be73c0e4063273
surevan08 <suryavan08@gmail.com>
2023-11-12 20:44:54 +0700

    slecing portfolio and home and make light mode

--- pages/index.vue
+++ pages/index.vue
@@ -59,3 +59,4 @@ onMounted(() => {
   });
 });
 </script> 
+ 
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,7 +1,130 @@
 <template>
   <div>
-    <div class="container">
-      <h1>Portfolio</h1>
+    <div class="portfolio">
+      <div class="container">
+        <h1>Our <span>Portfolio</span></h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sint
+          nesciunt minus iste a
+        </p>
+        <div class="row">
+          <div class="col-lg-4 mt-3">
+            <div class="card"> 
+                <img
+                  src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                /> 
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-3">
+            <div class="card">
+              <img
+                src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-3">
+            <div class="card">
+              <img
+                src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-3">
+            <div class="card">
+              <img
+                src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-3">
+            <div class="card">
+              <img
+                src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-3">
+            <div class="card">
+              <img
+                src="https://images.unsplash.com/photo-1545235617-9465d2a55698?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+              <div class="card-body">
+                <div class="text">
+                  <h5>Title Desc loream</h5>
+                  <a href="">
+                    <span> More Detail </span>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ul class="pagination_">
+          <li class="page_item active" aria-current="page">
+            <a class="page-link" href="#">1</a>
+          </li>
+          <li class="page_item">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page_item">
+            <a class="page-link" href="#">3</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -12,3 +135,4 @@ useHead({
   title: "Welcome - Surya Van",
 });
 </script>
+ 
